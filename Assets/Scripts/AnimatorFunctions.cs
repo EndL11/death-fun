e081fc13f7d5421d6332cff8ecd8ad548092a04b
Andriy Podobailo <podobailo.andriy@gmail.com>
2021-03-04 14:58:07 +0200

    fixed die animation, added event to attack animation, added script for animator events, added diyng animation by clicking right mouse button

---
+++ Assets/Scripts/AnimatorFunctions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+    public void StopAttack()
+    {
+        GetComponent<Animator>().SetBool("Attack", false);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator anim;
+    private float speed = 3f;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+            Move();
+        if (Input.GetMouseButtonDown(0))
+            Attack();
+        if (Input.GetMouseButtonDown(1))
+            Die();
+    }
+
+    private void Move()
+    {
+        //  get direction - left or right
+        float direction = Input.GetAxis("Horizontal");
+        //  move player according to direction
+        transform.Translate(transform.right * direction*  speed * Time.deltaTime);
+        //  flip x if moving left
+        GetComponentInChildren<SpriteRenderer>().flipX = direction > 0;
+
+    }
+
+    private void Attack()
+    {
+        anim.SetBool("Attack", false);
+        anim.SetBool("Attack", true);
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("Die");
+    }
+}
