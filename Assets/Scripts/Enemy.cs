3456438acbc3471c6eea41fd0558fd2787af6785
EndL11 <podobailo.andriy@gmail.com>
2021-03-05 18:22:23 +0200

    fixed collision betwen enemies, rewrited back pushing back enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,39 +24,19 @@ public class Enemy : MonoBehaviour
 
     public void ApplyDamage(float damage)
     {
+        PushBack();
         hp -= damage;
         if (hp <= 0)
             DestroyEnemy();
-        PushBack();
     }
 
     private void PushBack()
     {
-        //rb.velocity = Vector2.zero;
-        //rb.AddForce(new Vector2(transform.position.x + 0.1f, transform.position.y + 5f) * .4f, ForceMode2D.Impulse);
-
-        StartCoroutine(PushBackAnimation());
-
+        rb.velocity = Vector2.zero;
+        rb.AddForce(new Vector2(transform.position.x + 0.1f, transform.position.y + 5f) * .4f, ForceMode2D.Impulse);
         StartCoroutine(HurtAnimation());
     }
 
-    private IEnumerator PushBackAnimation()
-    {
-        Vector2 pos = transform.position;
-        float y = transform.position.y;
-        Vector2 target = new Vector2(pos.x + .3f, y + 0.3f);
-        while (Vector2.Distance(target, transform.position) >= 0.1f)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
-            yield return new WaitForSeconds(Time.deltaTime);
-        }
-        target = new Vector2(transform.position.x, transform.position.y - .3f);
-        while (Vector2.Distance(target, transform.position) >= 0.1f)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
-            yield return new WaitForSeconds(Time.deltaTime);
-        }
-    }
 
     private IEnumerator HurtAnimation()
     {
