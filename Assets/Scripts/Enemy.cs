3dc60793a5915e26544123fc39b104b49fd6d4fc
Andriy Podobailo <47588502+EndL11@users.noreply.github.com>
2021-03-05 15:01:32 +0200

    done animation for spider, done hurt animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -26,11 +26,21 @@ public class Enemy : MonoBehaviour
         if (hp <= 0)
             Destroy(gameObject);
         PushBack();
+        //GetComponentInChildren<SpriteRenderer>().material.color = new Color(255, 255, 255, 1f);
+
     }
 
     private void PushBack()
     {
         rb.velocity = Vector2.zero;
         rb.AddForce(new Vector2(transform.position.x + 0.1f, transform.position.y + 5f) * .4f, ForceMode2D.Impulse);
+        StartCoroutine(HurtAnimation());
+    }
+
+    private IEnumerator HurtAnimation()
+    {
+        GetComponentInChildren<SpriteRenderer>().material.color = new Color(255, 0, 0, .3f);
+        yield return new WaitForSeconds(0.2f);
+        GetComponentInChildren<SpriteRenderer>().material.color = new Color(255, 255, 255, 1f);
     }
 }
