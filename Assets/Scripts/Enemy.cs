4f4bfc635ffb6d2d6617709888bc044cc46ab108
MadiSan <dimande797@gmail.com>
2021-03-10 17:06:01 +0200

    redesigm level 2 and rewrite enemy collision

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -59,9 +59,9 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        if (!isPlayerNear() && isGrounded() && !isEndPlatform() && !dead)
+        if (!isPlayerNear() && isGrounded() && !isEndPlatform() && !dead && !isWall())
             Move();
-        else if(!dead && isGrounded() && isEndPlatform())
+        else if(!dead && isGrounded() && (isEndPlatform() || isWall()))
             ChangeMovementDirection();
 
         //  if delay greater zero
@@ -197,5 +197,12 @@ public class Enemy : MonoBehaviour
         //  return true if in player check zone at least 1 player object
         Collider2D[] colliders = Physics2D.OverlapCircleAll(checkPlayerPoint.position, playerCheckZone, whatIsPlayer);
         return colliders.Length != 0;
+    }
+
+    private bool isWall()
+    {
+        //  return true if in player check zone at least ground object
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(checkPlayerPoint.position, 0.1f, whatIsGround);
+        return colliders.Length > 0;
     }
 }
